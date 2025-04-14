459665552d89226b5b119b0713ecd82e5caf38ce
nmlynch94 <nmlynch94@gmail.com>
2025-04-14 02:44:04 -0400

    Add markdown LSP

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -649,6 +649,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
+        markdown_oxide = {},
         --
 
         terraformls = {},
