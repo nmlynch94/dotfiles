d8c74ba51faef73f4b88e3f103aa92d71be42b34
nmlynch94 <nmlynch94@gmail.com>
2025-04-15 21:32:27 -0400

    Fix media playback

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -827,6 +827,11 @@ require('lazy').setup({
         terraformls = { 'terraform_fmt' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
+        markdownlint = {
+          command = 'markdownlint',
+          args = { '-f', '$FILENAME' },
+          stdin = false,
+        },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
