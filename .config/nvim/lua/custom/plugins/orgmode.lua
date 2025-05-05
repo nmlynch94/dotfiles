286f4a8967e503dfa7e360cb850192b91bdbb06a
nmlynch94 <nmlynch94@gmail.com>
2025-05-05 03:03:37 -0400

    update

--- .config/nvim/lua/custom/plugins/orgmode.lua
+++ .config/nvim/lua/custom/plugins/orgmode.lua
@@ -7,6 +7,7 @@ return {
       require('orgmode').setup {
         org_agenda_files = '~/orgfiles/**/*',
         org_default_notes_file = '~/orgfiles/refile.org',
+        org_log_done = 'note',
       }
     end,
   },
